eb5a3fc2303e85170f29718117922566f5c385bc
Cha Yang <chachaiyang3@gmail.com>
2019-07-20 22:02:06 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", 
+"kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  return array[element[0]];
+}
+addElementToBeginningOfArray([1], 'foo');
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+destructivelyAddElementToBeginningOfArray([1], 'foo');
+function addElementToEndOfArray(array, element){
+  return array.push(element);
+}
+addElementToEndOfArray([1], 'foo');
