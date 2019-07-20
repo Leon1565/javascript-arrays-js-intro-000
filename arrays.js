29bf4aabe764ea7b070f4e040ec74f9bc0cd53ba
Cha Yang <chachaiyang3@gmail.com>
2019-07-20 23:57:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,6 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
   return chocolateBars;
 }
 function addElementToEndOfArray(chocolateBars, element){
-  chocolateBars[element];
+  chocolateBars.push(element);
   return chocolateBars;
 }
