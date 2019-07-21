dc2a39e3a15c42782993328ce377ac0f902b3509
Cha Yang <chachaiyang3@gmail.com>
2019-07-21 00:32:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   return chocolateBars;
 }
 function removeElementFromEndOfArray(chocolateBars){
-  return chocolateBars.slice(chocolateBars.length + 1);
+  return chocolateBars.slice(chocolateBars.length - 1);
 }
 
 
